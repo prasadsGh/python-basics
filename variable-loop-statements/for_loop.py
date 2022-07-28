00bc9ca3f9c7719f73493fa660f8b6832633a825
prasad patil <prasadpatil2309@gmail.com>
2022-07-28 17:49:09 +0530

    basic codes

--- variable-loop-statements/for_loop.py
+++ variable-loop-statements/for_loop.py
@@ -1,5 +1,6 @@
 #creation of list in python 
-exampleList=[1,2,3,4,5]
+exampleList=[10,9,8,7,6,5,4,3,2,1,0]
+
 for x in exampleList:
      print(x)
      
@@ -10,3 +11,18 @@ print("-------------------------------------------------------------------------
 #for printing simple loops 
 for i in range (1,10):
     print(i)
+print("-----------------------------------------------------------------------------")
+    
+#for printing specific ranges only
+# range(stop)
+# range(start, stop[, step])
+ 
+for i in range(1,10,2):
+     print(exampleList[i])
+print("-----------------------------------------------------------------------------")
+#in next code we just stop at the given value 
+for i in range(10):
+    print(i)
+print("-----------------------------------------------------------------------------")
+for i in range(5):
+    print(exampleList[i])
