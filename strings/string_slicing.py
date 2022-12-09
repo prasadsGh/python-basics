f603d835c6fd9a654a9fd0eba29bffb78d89cc8b
prasad patil <prasadpatil2309@gmail.com>
2022-12-09 22:20:31 +0530

    Code

---
+++ strings/string_slicing.py
@@ -0,0 +1,16 @@
+#length of the string 
+a="prasad patil"
+print(len(a))
+
+print("My name is ",a)
+#string slicing 
+
+#string_name[starting index:ending index]
+#ending index is exclusive
+
+print(a[0:200])
+print(a[0:6])
+print(a[:6]) #same as above line 
+print(a[-1:6]) #no output
+print(a[-3:-1])
+print(a[-3:0])#no output
